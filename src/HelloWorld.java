e23b1c5d7b598c62d5fc0bfa35844e16e25bc91d
Samuel Prashker <github@prashker.net>
2014-01-14 18:22:42 -0500

    Layout fix.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World - KingsSGF - To be Replaced");
+		System.out.println("Layout Folder Fix");
 	}
 
 }
