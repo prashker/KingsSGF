b6ead9e2aba9e6c21c2bbf4d4b9b80078a195337
Samuel Prashker <github@prashker.net>
2014-02-06 11:36:14 -0500

    This class is going to make me die....also Hex View stuff

--- src/uiSam/HexGridView.java
+++ src/uiSam/HexGridView.java
@@ -1,9 +1,12 @@
 package uiSam;
 
+import java.util.Observable;
+
+import hexModelSam.HexGrid;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 
-public class HexGridView extends AnchorPane {
+public class HexGridView extends AnchorPane implements KingsAndThingView<HexGrid> {
 
 	//All 37?
 	@FXML private HexTileView hexOne;
@@ -11,5 +14,26 @@ public class HexGridView extends AnchorPane {
 	
 	public void initialize() {
 		System.out.println("Hexgrid initialize");
+	}
+
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void setBind(HexGrid m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void updateUI() {
+		// TODO Auto-generated method stub
+		
 	}
 }
--- src/uiSam/HexTileView.java
+++ src/uiSam/HexTileView.java
@@ -1,11 +1,33 @@
 package uiSam;
 
+import java.util.Observable;
+
+import hexModelSam.HexModel;
 import javafx.scene.image.ImageView;
 
-public class HexTileView extends ImageView {
+public class HexTileView extends ImageView implements KingsAndThingView<HexModel> {
 
 	
 	public void initialize() {
 		System.out.println("Hextile initialized");
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		o.
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setBind(HexModel m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateUI() {
+		// TODO Auto-generated method stub
+		
 	}
 }
